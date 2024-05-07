dfedaa2d03a2c8e84db21085ec48625c5a48f74a
Profess0ro <professoro88@gmail.com>
2024-05-07 09:50:37 +0200

    changed message when declining a deletion of an item

--- run.py
+++ run.py
@@ -286,7 +286,8 @@ def delete_item_on_packing_list(worksheet):
                     print(Fore.GREEN + "deleted successfully.")
                 elif confirm.lower() == "n":
                     clear()
-                    print("Deletion canceled.")
+                    print(Fore.RED + 
+                          f"Deletion of {items_list[item_index - 1]}canceled.")
                 else:
                     clear()
                     print(Fore.RED + f"{confirm} invalid")
